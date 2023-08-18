7c09fa59dba5c8cffe13f05d55c768762fdd0a98
NahuelBOR <elnau94@gmail.com>
2023-08-17 23:47:05 -0300

    --

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+import { log } from 'console';
 import express from 'express'
+import {promises as fs} from 'fs'
 
 const productos = [
     {
         nombre: 'Coca Cola',
-        id: 1,
+        id: 1, 
         categoria: "gaseosa"
     },
     {
@@ -19,11 +21,88 @@ const productos = [
 ]
 
 const app = express();
-
 const PORT = 4000;
+const PATH = './src/users.json'
+
+class User {
+    constructor(nombre, apellido, email, password) {
+        this.nombre  =  nombre;
+        this.apellido  =  apellido;
+        this.email  =  email;
+        this.password  =  password;
+        this.id = User.incrementarId();
+    }
+
+    static incrementarId(){
+        if(this.idIncrement){
+            this.idIncrement = this.idIncrement + 1;
+        }else{
+            this.idIncrement = 1;
+        }
+
+        return this.idIncrement
+    }
+}
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
 
 app.get('/', (req, res) => {
     res.send('Hola desde la pagina de inicio de mi app')
+})
+
+app.post('/users', async (req, res) => {
+    const {nombre, apellido, email, password } = req.body
+    console.log(req.body);
+
+    const users = JSON.parse(await fs.readFile(PATH, 'utf-8'))
+    const user = users.find(usuario => usuario.email === email)
+
+    if(user){
+        res.status(400).send('Usuario ya existe')
+    }else{
+        const userClass = new User(nombre, apellido, email, password)
+        users.push(userClass)
+        await fs.writeFile(PATH, JSON.stringify(users))
+
+        res.status(200).send(`Usuario ${nombre} Creado`)
+    }
+})
+
+app.put('/users/:id', async (req, res) => {
+    const { id } = req.params
+    const { nombre, apellido, email, password } = req.body
+    console.log(req.body);
+
+    const users = JSON.parse(await fs.readFile(PATH, 'utf-8'))
+    const userIndex = users.findIndex(usuario => usuario.id === parseInt(id))
+
+    if(userIndex != -1){
+        users[userIndex].nombre = nombre
+        users[userIndex].apellido = apellido
+        users[userIndex].email = email
+        users[userIndex].password = password
+        await fs.writeFile(PATH, JSON.stringify(users))
+
+        res.status(200).send(`Usuario ${nombre} Actualizado`)
+    }else{
+        res.status(400).send('Usuario no existe')
+    }
+})
+
+app.delete('/users/:id', async (req, res) => {
+    const { id } = req.params
+
+    const users = JSON.parse(await fs.readFile(PATH, 'utf-8'))
+    const userIndex = users.findIndex(usuario => usuario.id === parseInt(id))
+
+    if(userIndex != -1){
+        await fs.writeFile(PATH, JSON.stringify(users.filter(user => user.id != parseInt(id))))
+        res.status(200).send(`Usuario Eliminado`)
+    }else{
+        res.status(400).send('Usuario no existe')
+    }
 })
 
 app.get('/producto', async (req, res) => {
