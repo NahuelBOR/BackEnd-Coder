449378c1de9fbebfd431f8bdc93839d6393c68e5
NahuelBOR <elnau94@gmail.com>
2023-08-22 22:36:31 -0300

    ..-

--- src/index.js
+++ src/index.js
@@ -1,108 +1,41 @@
 import { log } from 'console';
-import express from 'express'
+import routerProd from './routes/product.routes.js';
+import express, { response } from 'express'
+import multer from 'multer';
 import {promises as fs} from 'fs'
-
-const productos = [
-    {
-        nombre: 'Coca Cola',
-        id: 1, 
-        categoria: "gaseosa"
-    },
-    {
-        nombre: 'Pepsi',
-        id: 2,
-        categoria: "gaseosa"
-    },
-    {
-        nombre: 'Gallo Oro',
-        id: 3,
-        categoria: "arroz"
-    },
-]
+import { __dirname } from './path.js'
 
 const app = express();
 const PORT = 4000;
-const PATH = './src/users.json'
 
-class User {
-    constructor(nombre, apellido, email, password) {
-        this.nombre  =  nombre;
-        this.apellido  =  apellido;
-        this.email  =  email;
-        this.password  =  password;
-        this.id = User.incrementarId();
+//Config
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => { //cb => callback
+        cb(null, 'src/public/img')
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}${file.originalname}`)
     }
+})
 
-    static incrementarId(){
-        if(this.idIncrement){
-            this.idIncrement = this.idIncrement + 1;
-        }else{
-            this.idIncrement = 1;
-        }
-
-        return this.idIncrement
-    }
-}
-
+//Middlewares
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+const upload = multer({ storage: storage})
+
+//Routes
+app.use('/static', express.static(__dirname + '/public'))
+app.use('/api/product', routerProd)
+app.post('/upload', upload.single('product'), (req, res) => {
+    console.log(req.file);
+    console.log(req.body);
+    res.status(200).send("imagen Cargada")
+})
 
 
 app.get('/', (req, res) => {
     res.send('Hola desde la pagina de inicio de mi app')
-})
-
-app.post('/users', async (req, res) => {
-    const {nombre, apellido, email, password } = req.body
-    console.log(req.body);
-
-    const users = JSON.parse(await fs.readFile(PATH, 'utf-8'))
-    const user = users.find(usuario => usuario.email === email)
-
-    if(user){
-        res.status(400).send('Usuario ya existe')
-    }else{
-        const userClass = new User(nombre, apellido, email, password)
-        users.push(userClass)
-        await fs.writeFile(PATH, JSON.stringify(users))
-
-        res.status(200).send(`Usuario ${nombre} Creado`)
-    }
-})
-
-app.put('/users/:id', async (req, res) => {
-    const { id } = req.params
-    const { nombre, apellido, email, password } = req.body
-    console.log(req.body);
-
-    const users = JSON.parse(await fs.readFile(PATH, 'utf-8'))
-    const userIndex = users.findIndex(usuario => usuario.id === parseInt(id))
-
-    if(userIndex != -1){
-        users[userIndex].nombre = nombre
-        users[userIndex].apellido = apellido
-        users[userIndex].email = email
-        users[userIndex].password = password
-        await fs.writeFile(PATH, JSON.stringify(users))
-
-        res.status(200).send(`Usuario ${nombre} Actualizado`)
-    }else{
-        res.status(400).send('Usuario no existe')
-    }
-})
-
-app.delete('/users/:id', async (req, res) => {
-    const { id } = req.params
-
-    const users = JSON.parse(await fs.readFile(PATH, 'utf-8'))
-    const userIndex = users.findIndex(usuario => usuario.id === parseInt(id))
-
-    if(userIndex != -1){
-        await fs.writeFile(PATH, JSON.stringify(users.filter(user => user.id != parseInt(id))))
-        res.status(200).send(`Usuario Eliminado`)
-    }else{
-        res.status(400).send('Usuario no existe')
-    }
 })
 
 app.get('/producto', async (req, res) => {
@@ -126,6 +59,7 @@ app.get('/producto/:id', async (req, res) => {
     }
 })
 
+//Server
 app.listen(PORT, () => {
     console.log(`Server on port ${PORT}`);
 })
---
+++ src/routes/product.routes.js
@@ -0,0 +1,49 @@
+import  { Router } from 'express'
+import { ProductManager } from '../controllers/produsctManager.js'
+
+const productManager = new ProductManager('src/models/productos.txt')
+
+const routerProd = Router()
+
+routerProd.get('/', async(req, res) => {
+    const {limit} = req.query
+
+    const prods = await productManager.getProducts()
+    const productos = prods.slice(0, limit)
+    res.status(200).send(productos)
+})
+routerProd.get('/:id', async(req, res) => {
+    const {id} = req.params
+    const prod = await productManager.getProductByIs(id)
+
+    if(prod)
+        res.status(200).send(prod)
+    else
+        res.status(400).send('No existe')
+})
+routerProd.post('/', async(req, res) => {
+    const confirmacion = await productManager.addProduc(req.body)
+
+    if(confirmacion)
+        res.status(200).send('Producto creado corectamente')
+    else
+        res.status(404).send('Producto ya existente')
+})
+routerProd.put('/:id', async(req, res) => {
+    const confirmacion = await productManager.updateProduct(req.params.id, req.body)
+
+    if(confirmacion)
+        res.status(200).send('Producto actualizado corectamente')
+    else
+        res.status(404).send('Producto no encontrado')
+})
+routerProd.delete('/:id', async(req, res) => {
+    const confirmacion = await productManager.deleteProduct(req.params.id)
+
+    if(confirmacion)
+        res.status(200).send('Producto eliminado corectamente')
+    else
+        res.status(404).send('Producto no encontrado')
+})
+
+export default routerProd
